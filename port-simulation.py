d90b1f40a667e5267522944a571e0b5d319e1f62
Daniel Castro <danielcastrofafe@gmail.com>
2021-04-21 18:25:03 +0100

    Minor tweaks.

--- port-simulation.py
+++ port-simulation.py
@@ -8,8 +8,8 @@ class ShipException(Exception):
 
 
 class Ship:
-    TIME_UNLOADING_CARGO = 5  # ! Random Number
-    TIME_REFUEL = 10  # ! Random Number
+    TIME_UNLOADING_CARGO = random.randint(1, 5)  # ! Random Number
+    TIME_REFUEL = random.randint(1, 10)  # ! Random Number
 
     def __init__(self, env, port, name):
         self.env, self.port, self.name = env, port, name
@@ -22,68 +22,71 @@ class Ship:
         yield self.env.timeout(Ship.TIME_REFUEL)
 
     def run_life_cicle(self):
-        print(f"@{self.env.now} - {self.name}: Arrived At Port")
-        arrived = self.env.event()
-        self.port.service_line.append(arrived)
+        print(f"@{self.env.now} - {self.name}: Arrived at port.")
+        dock = self.env.event()
+        self.port.unloading_service_line.append(dock)
 
         if self.port.idle:
             self.port.process.interrupt()
 
         try:
-            yield arrived
-            print(f"@{self.env.now} - Ship can unload")
+            print(f"@{self.env.now} - Ship is waiting for an unloading dock.")
+            yield dock
 
-            with self.port.unloading_station.request() as req:
-                print(f"@{self.env.now} - {self.name}: Arrived Unloading Station")
-                yield req
-                print(f"@{self.env.now} - {self.name}: Started Unloading.")
+            with self.port.unloading_station.request() as dock:
+                print(
+                    f"@{self.env.now} - {self.name}: Arrived at unloading station dock.")
+                yield dock
+                print(f"@{self.env.now} - {self.name}: Started unloading.")
                 yield from self.unload_cargo()
-                print(f"@{self.env.now} - {self.name}: Finnised Unloading.")
+                print(f"@{self.env.now} - {self.name}: Finnised unloading.")
 
-                with self.port.fueling_station.request() as req:
-                    print(f"@{self.env.now} - {self.name}: Arrived Fuel Station")
-                    yield req
-                    print(f"@{self.env.now} - {self.name}: Started Fueling.")
+                with self.port.fueling_station.request() as dock:
+                    print(
+                        f"@{self.env.now} - {self.name}: Arrived at fuel station dock.")
+                    yield dock
+                    print(f"@{self.env.now} - {self.name}: Started fueling.")
                     yield from self.unload_cargo()
-                    print(f"@{self.env.now} - {self.name}: Finnised Fueling.")
+                    print(f"@{self.env.now} - {self.name}: Finnised fueling.")
         except ShipException:
-            print(f"@{self.env.now} - Ship cannot unload")
+            print(f"@{self.env.now} - Ship cannot get an unloading dock.")
 
 
 class Port:
     def __init__(self, env, unloading_station, fueling_station):
         self.env, self.unloading_station, self.fueling_station = env, unloading_station, fueling_station
         self.idle = False
-        self.service_line = deque()
+        self.unloading_service_line = deque()
+        self.fueling_service_line = deque()  # !!! Implementar com a queue para descarga
         self.process = env.process(self.run_life_cicle())
 
     def run_life_cicle(self):
         while True:
-            if self.service_line:
-                ship_arrived = self.service_line.popleft()
+            if self.unloading_service_line:
+                ship_arrived = self.unloading_service_line.popleft()
 
-                with self.unloading_station.request() as ticket:
+                with self.unloading_station.request() as dock:
                     try:
-                        yield ticket
+                        yield dock
                         ship_arrived.succeed()
                     except simpy.Interrupt:
                         ship_arrived.fail(ShipException())
 
             else:
                 self.idle = True
-                print(f"@{self.env.now} - Port waiting for Ships.")
+                # print(f"@{self.env.now} - Port is waiting for Ships.")
                 try:
                     yield self.env.event()
                 except simpy.Interrupt:
                     self.idle = False
-                    print(f"@{self.env.now} - Ships arrived at Port.")
+                    # print(f"@{self.env.now} - Ships are waiting for service.")
 
 
 def generate_ships(env, unloading_station, fueling_station):
     i = 0
     ship_inter_arrival_time = 10  # ! Random Number
     while True:
-        yield env.timeout(random.randint(1, ship_inter_arrival_time))
+        yield env.timeout(random.randint(0, ship_inter_arrival_time))
         i += 1
         Ship(env, Port(env, unloading_station,
              fueling_station), name=f"Ship {i}")
@@ -103,6 +106,6 @@ def main(time):
 
 if __name__ == "__main__":
 
-    time = 5000
+    time = 100
 
     main(time)
