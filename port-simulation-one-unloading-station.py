cf604ce61b8bbbf33a7a596ac6abbb604e7e728a
Daniel Castro <danielcastrofafe@gmail.com>
2021-04-25 14:54:51 +0100

    Added history and graphs for waiting times and lenght of queues.

--- port-simulation-one-unloading-station.py
+++ port-simulation-one-unloading-station.py
@@ -2,6 +2,7 @@
 import simpy
 import numpy
 import random
+import matplotlib.pyplot as plt
 from collections import deque
 
 
@@ -11,12 +12,20 @@ class ShipException(Exception):
 
 class Ship:
 
-    def __init__(self, env, port,  arrival_time, unload_time, refuel_time, name):
+    def __init__(self, env, port, unload_time, refuel_time, name):
         self.env, self.port, self.name = env, port, name
-        self.arrival_time, self.unload_time, self.refuel_time, = arrival_time, unload_time, refuel_time
+        self.unload_time, self.refuel_time, =  unload_time, refuel_time
         self.waiting_unload_time, self.waiting_refuel_time = 0, 0
         self.unloaded, self.refueled = False, False
         self.env.process(self.run_life_cicle())
+
+    def enter_unloading_queue(self):
+        self.port.unloading_service_line_history.append(
+            [self.env.now, len(self.port.unloading_service_line)])
+
+    def enter_refueling_queue(self):
+        self.port.refueling_service_line_history.append(
+            [self.env.now, len(self.port.refueling_service_line)])
 
     def unload_cargo(self, dock):
         # print(f"@{self.env.now} - {self.name}: Started unloading.")
@@ -39,10 +48,11 @@ class Ship:
             self.port.process.interrupt()
 
         try:
-            if self.unloaded != True:
+            if self.unloaded == False:
                 # ! Enter Unload Queue
                 unloading_queue = self.env.event()
                 self.port.unloading_service_line.append(unloading_queue)
+                self.enter_unloading_queue()
                 start_unloading_waiting = self.env.now
                 yield unloading_queue
 
@@ -53,12 +63,12 @@ class Ship:
                     # ! Started Unload
                     yield from self.unload_cargo(unload_dock)
 
-            if self.unloaded == True and self.refueled != True:
+            if self.unloaded == True and self.refueled == False:
                 # ! Enter Refuel Queue
                 refuel_queue = self.env.event()
                 self.port.refueling_service_line.append(refuel_queue)
+                self.enter_refueling_queue()
                 start_refueling_waiting = self.env.now
-
                 with self.port.refueling_station.request() as refuel_dock:
                     # ! Waiting for Refuel Dock
                     yield refuel_dock
@@ -74,8 +84,8 @@ class Port:
     def __init__(self, env, unloading_station, refueling_station):
         self.env, self.unloading_station, self.refueling_station = env, unloading_station, refueling_station
         self.idle = False
-        self.unloading_service_line = deque()
-        self.refueling_service_line = deque()
+        self.unloading_service_line, self.unloading_service_line_history = deque(), []
+        self.refueling_service_line, self.refueling_service_line_history = deque(), []
         self.process = env.process(self.run_life_cicle())
 
     def run_life_cicle(self):
@@ -107,6 +117,7 @@ class Port:
 def generate_ships(env, time, unloading_station, refueling_station):
     i = 0
     ships = []
+    port = Port(env, unloading_station, refueling_station)
 
     while True:
 
@@ -119,49 +130,80 @@ def generate_ships(env, time, unloading_station, refueling_station):
         refuel_time = numpy.random.choice(numpy.arange(9, 17), p=[
             0.2, 0.22, 0.19, 0.16, 0.1, 0.08, 0.03, 0.02])
 
-        if (env.now+arrival_time) > time:
-            unloaded_ships = []
-            refueled_ships = []
-            for ship in ships:
-                if ship.unloaded:
-                    unloaded_ships.append(ship)
+        if (env.now + arrival_time) >= time:
+            return ships, port
+        else:
+            yield env.timeout(random.randint(0, arrival_time))
+            i += 1
+            ships.append(Ship(env, port, unload_time,
+                         refuel_time, name=f"Ship {i}"))
 
-                if ship.refueled:
-                    refueled_ships.append(ship)
 
-            return unloaded_ships, refueled_ships
+def stats_and_graphs(ships, port):
+    plt.figure(1)
+    filename = "img/unload.jpg"
 
-        yield env.timeout(random.randint(0, arrival_time))
-        i += 1
-        ships.append(Ship(env, Port(env, unloading_station, refueling_station),
-                          arrival_time, unload_time, refuel_time, name=f"Ship {i}"))
+    Y = [(ship.waiting_unload_time)
+         for ship in ships if ship.unloaded == True]
+    X = range(0, len(Y))
+
+    plt.plot(X, Y, "-o")
+    plt.xlabel("Number of ships")
+    plt.ylabel("Waiting time for unloading")
+    plt.xticks(X)
+    plt.savefig(filename)
+    #!!! ----------
+    plt.figure(2)
+    filename = "img/refuel.jpg"
+    Y = [(ship.waiting_refuel_time)
+         for ship in ships if ship.refueled == True]
+    X = range(0, len(Y))
+
+    plt.plot(X, Y, "-o")
+    plt.xlabel("Number of ships")
+    plt.ylabel("Waiting time for unloading")
+    plt.xticks(X)
+    plt.savefig(filename)
+    #!!! ----------
+    plt.figure(3)
+    filename = "img/unload_queue.jpg"
+    X = [timestamp[0]for timestamp in port.unloading_service_line_history]
+    Y = [timestamp[1]for timestamp in port.unloading_service_line_history]
+
+    plt.plot(X, Y, "-o")
+    plt.xlabel("Time")
+    plt.ylabel("Unloading Queue Lenght")
+    plt.xticks(X)
+    plt.savefig(filename)
+    #!!! ----------
+    plt.figure(4)
+    filename = "img/refuel_queue.jpg"
+    X = [timestamp[0]for timestamp in port.refueling_service_line_history]
+    Y = [timestamp[1]for timestamp in port.refueling_service_line_history]
+
+    plt.plot(X, Y, "-o")
+    plt.xlabel("Time")
+    plt.ylabel("Refueling Queue Lenght")
+    plt.xticks(X)
+    plt.savefig(filename)
 
 
 def simulation(env, time, number_of_unloading_stations, number_of_refueling_stations):
     unloading_stations = simpy.Resource(env, number_of_unloading_stations)
     refueling_stations = simpy.Resource(env, number_of_refueling_stations)
-    ships, ships2 = yield env.process(generate_ships(env, time, unloading_stations, refueling_stations))
-
-    print("Unloaded Ships")
-    for ship in ships:
-        print(f"@{ship.name} - U: {ship.unloaded} - R:{ship.refueled}")
-
-    print("------------------")
-    print("Refueled Ships")
-    for ship in ships2:
-        print(f"@{ship.name} - U: {ship.unloaded} - R:{ship.refueled}")
+    ships, port = yield env.process(generate_ships(env, time, unloading_stations, refueling_stations))
+    stats_and_graphs(ships, port)
 
 
 def main(time, number_of_unloading_stations, number_of_refueling_stations):
     env = simpy.Environment()
     env.process(simulation(env, time, number_of_unloading_stations,
                 number_of_refueling_stations))
-
-    env.run(until=time+1)
+    env.run(until=(time + 5))
 
 
 if __name__ == "__main__":
-    time = 1000
-    number_of_unloading_stations = 1
+    time = 100
+    number_of_unloading_stations = 3
     number_of_refueling_stations = 1
     main(time, number_of_unloading_stations, number_of_refueling_stations)
